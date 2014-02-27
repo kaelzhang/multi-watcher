14c773da34f8326ec5e859c737474cdd0c66796a
kaelzhang <i@kael.me>
2014-02-27 15:52:52 +0800

    add comments

--- test/stares.js
+++ test/stares.js
@@ -3,6 +3,7 @@
 var expect = require('chai').expect;
 var stares = require('../');
 
+// Oh, forgive me, it will be killing me to write test cases with multiple processes.
 
 describe("master", function(){
     it("watch", function(done){
